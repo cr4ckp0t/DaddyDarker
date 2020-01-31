0dcf911750f8608fd471286d2b049d663b209d4b
Adam Koch <me@adamko.ch>
2020-01-30 23:52:05 -0500

    1.0.1 Release

--- DaddyDarker.lua
+++ DaddyDarker.lua
@@ -14,8 +14,19 @@ DD.version = GetAddOnMetadata("DaddyDarker", "Version")
 local defaults = {
 	global = {
 		inviting = false,
+		players = {}
 	}
 }
+
+local function FindInArray(toFind, arraySearch)
+	if #arraySearch == 0 then return false end
+	for _, value in pairs(arraySearch) do
+		if value == toFind then
+			return true
+		end
+	end
+	return false
+end
 
 function DD:Print(msg)
 	print(("|cffC41F3BDaddyDarker|r |cffffffff%s|r"):format(msg))
@@ -24,9 +35,10 @@ end
 function DD:CHAT_MSG_GUILD(evemt, msg, player, ...)
 	local temp = {strsplit("-", player)}
 	local playerName = temp[1]
-	if self.db.global.inviting and msg:match("[xX]+") and playerName:lower() ~= UnitName("player"):lower() then
+	if self.db.global.inviting and msg:match("[xX]+") and playerName:lower() ~= UnitName("player"):lower() and not FindInArray(playerName:lower(), self.db.global.players) then
 		self:Print(("Inviting %s"):format(playerName))
 		C_PartyInfo_InviteUnit(playerName)
+		self.db.global.players[#self.db.global.players + 1] = playerName:lower()
 	end
 end
 
@@ -44,6 +56,10 @@ function DD:OnInitialize()
 	self:RegisterChatCommand("darker", function(args)
 		self.db.global.inviting = not self.db.global.inviting
 		self:Print(self.db.global.inviting and "Raid invites have started!" or "Raid invites have stopped!")
-		if self.db.global.inviting then SendChatMessage("Type XX for a raid invite!", "GUILD") end
+		if self.db.global.inviting then
+			SendChatMessage("Type XX for a raid invite!", "GUILD")
+		else
+			self.db.global.players = {}
+		end
 	end)
 end
